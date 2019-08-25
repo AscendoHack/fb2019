6f1b3a2e907bb7f740c572fea85180d0027d6fa3
KEMPRUT <52598832+AscendoHack@users.noreply.github.com>
2019-08-25 19:36:29 +0700

    Add files via upload

---
+++ Fb25.sh
@@ -0,0 +1,239 @@
+#!/bin/bash
+clear
+echo "MOHON TUNGGU...."
+sleep 2
+clear
+toilet -f standard ENTUP ID -F gay
+echo
+echo "
+╔═════════════════════════════════════════╗
+║ Author  : TU4N3NTUP                     ║
+║ Youtube : ENTUP ID                      ║
+║ github  : https://github.com/KEMPRUT    ║
+║ Facebook: ENTUP ID                      ║
+╚═════════════════════════════════════════╝" | lolcat
+echo
+echo " JANGAN LUPA SUBSCRIBE CHANNEL AUTHOR"
+echo " NAME CHANNEL : ENTUP ID "
+echo "
+=================================
+[1]. BRUTEFORCE...
+=================================
+[2]. HACK TARGET...
+=================================
+[3]. CRACK FB...
+=================================
+[4]. FAKE FB...
+=================================
+[5]. AUTO MBF...
+=================================
+[6]. DARK MOD2.....
+=================================
+[7]. DARK 4...
+=================================
+[8]. DARK VIP V1.9....
+=================================
+[9]. DARK TARGET...
+=================================
+[10]. HACK FBH...
+=================================
+[11]. HACK FB MASSAL...
+=================================
+[12]. DARK FB V1.6...
+=================================
+[13]. DARK FB V1.7...
+=================================
+[14]. DARK VVIP NEW...
+=================================
+[15]. DARK PREMIUM NEW...
+=================================
+[99]. EXIT PROGRAM...
+================================" | lolcat
+echo
+read -p "   root@anonymousindo =》 " pil
+
+if [ $pil = 1 ] || [ $pil = 01 ]
+then
+clear
+pkg install php -y
+pkg install git -y
+pkg install python2 -y
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+cd BRUTEFORCEnew
+sh new.sh
+fi
+
+if [ $pil = 2 ] || [ $pil = 02 ]
+then
+clear
+pkg install git
+git clone https://github.com/DarkCurut08/DarkCurut08
+ls
+cd DarkCurut08
+ls
+sh Moreno77.sh
+fi
+
+if [ $pil = 3 ] || [ $pil = 03 ]
+then
+clear
+pkg install python2
+pkg install git
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/EgiRecod/crackfb
+cd crackfb
+python2 crack.py
+fi
+
+if [ $pil = 4 ] || [ $pil = 04 ]
+then
+clear
+pkg install python2
+pkg install git
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/EgiRecod/FakeFB
+cd FakeFB
+python2 fake.py
+fi
+
+if [ $pil = 5 ] || [ $pil = 05 ]
+then
+clear
+pkg update && pkg upgrdae -y
+pkg install python2 -y
+pkg install git -y
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/unikers71/autombf
+cd autombf
+python2 mbf.py
+fi
+
+if [ $pil = 6 ] || [ $pil = 06 ]
+then
+clear
+pkg upgrade && pkg update
+pkg install python2
+pip2 install requests
+pip2 install mechanize
+pkg install git
+git clone https://github.com/cyber2611/cyber2000
+ls
+cd cyber2000
+ls
+python2 Dark2.py
+fi
+
+if [ $pil = 7 ] || [ $pil = 07 ]
+then
+clear
+ pkg update
+pkg upgrade
+pkg install python2
+pip2 install requests
+pip2 install mechanize
+pkg install git
+git clone https://github.com/cyber2611/Dark4
+ls
+cd Dark4
+ls
+python2 Dark4.py
+fi
+
+if [ $pil = 8 ] || [ $pil = 08 ]
+then
+clear
+pkg install python2
+pip2 install mechanize
+pip2 install requests
+pkg install git
+git clone https://github.com/cyber2611/BHV26
+ls
+cd BHV26
+ls
+python2 vip-1.9.py
+fi
+
+if [ $pil = 9 ] || [ $pil = 09 ]
+then
+clear
+pkg update && pkg upgrade
+pkg install python2
+pip2 install mechanize
+pkg install git
+git clone https://github.com/gellmoxer/gm13
+cd gm13                                                                     python2 gm13.py
+fi
+
+if [ $pil = 10 ] || [ $pil = 10 ]
+then                                                                        clear
+pkg update && pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/gellmoxer/FBI
+cd FBI
+python2 fbi.py
+fi
+
+if [ $pil = 11 ] || [ $pil = 11 ]
+then
+clear
+pkg update && pkg upgrade
+pkg install python2
+pip2 install mechanize
+pkg install git
+git clone https://github.com/gellmoxer/reggae
+cd reggae
+python2 reggae.py
+fi
+
+if [ $pil = 12 ] || [ $pil = 12 ]
+then
+clear
+pkg install git
+pkg install python2
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+pip2 install requests
+pip2 install mechanize
+python2 SETAN.py
+fi
+
+if [ $pil = 13 ] || [ $pil = 13 ]
+then
+clear
+pkg install git
+pkg install python2
+git clone https://github.com/TheMagizz/fb1.7
+cd fb1.7
+pip2 install requests mechanize
+python2 dark.py
+fi
+
+if [ $pil = 14 ] || [ $pil = 14 ]
+then
+clear                                                                                   pkg install git
+pkg install python2
+git clone https://github.com/TheMagizz/DarkVip
+cd DarkVip
+pip2 install -r requirements.txt
+python2 darkvip.py
+fi
+
+if [ $pil = 15 ] || [ $pil = 15 ]
+then                                                                                    clear
+pkg install git
+pkg install python2
+git clone https://github.com/TheMagizz/DarkPremium
+cd DarkPremium
+pip2 install -r requirements.txt
+python2 DarkFB.py
+fi
+
+if [ $pil = 00 ] || [ $pil = 00 ]
+then
+echo " THANKS SUDAH GUNAKAN TOOLS INI"
+echo " JANGAN LUPA SUBSCRIBE CHANNEL ENTUP ID "
+
